6da0f6d7e007fc818b707544e871e02cf2eb8788
idkagooduser <98674911+idkagooduser@users.noreply.github.com>
2023-10-15 11:48:16 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.position(600,100);
+
+    video=createCapture(VIDEO);
+    video.size(500,400);
+    video.position(20,50)
+
+};
+
+function draw(){
+    background("teal");
+} 
