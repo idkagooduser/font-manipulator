f662fe7f89ca1278cc18cf73e28f8818bedac173
idkagooduser <98674911+idkagooduser@users.noreply.github.com>
2023-10-22 10:47:23 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 function setup(){
-    canvas=createCanvas(500,500);
-    canvas.position(600,100);
+    canvas=createCanvas(500,450);
+    canvas.position(850,150);
 
     video=createCapture(VIDEO);
-    video.size(500,400);
-    video.position(20,50)
+    video.size(500,600);
+    video.position(200,50);
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
 
 };
 
+function modelLoaded(){
+    console.log("model is ready");
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+}
+}
+
+
 function draw(){
-    background("teal");
+    background(76, 111, 140);
 } 
