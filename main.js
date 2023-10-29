106966c565b8843199218dcb346a9ea24e4dc3c1
idkagooduser <98674911+idkagooduser@users.noreply.github.com>
2023-10-29 11:42:27 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+var noseX=0;
+var noseY=0;
+var leftWristX=0;
+var rightWristX=0;
+var diff=0;
+
 function setup(){
-    canvas=createCanvas(500,450);
-    canvas.position(850,150);
+    canvas=createCanvas(500,500);
+    canvas.position(600,100);
 
     video=createCapture(VIDEO);
     video.size(500,600);
-    video.position(200,50);
+    video.position(20,50);
 
     poseNet=ml5.poseNet(video, modelLoaded);
     poseNet.on('pose',gotPoses);
@@ -18,10 +24,22 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length>0){
     console.log(results);
+
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    diff=floor(leftWristX-rightWristX);
 }
 }
 
 
 function draw(){
-    background(76, 111, 140);
+    document.getElementById("size").innerHTML="Width and height of the text: "+diff +" pixels";
+    background(79,11,50);
+stroke(76,80,90);
+fill(76,80,90);
+textSize(diff);
+textAlign(CENTER,CENTER);    
+text("YIPEE",noseX,noseY);
 } 
